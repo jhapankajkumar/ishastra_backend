d8da9cc90538bbdc626cefb50084d0a57f2211f1
Pankaj Kumar Jha <pankajkumar.jha@income.com.sg>
2025-09-29 23:03:42 +0800

    Break Even Check

--- src/services/alert.service.js
+++ src/services/alert.service.js
@@ -82,7 +82,8 @@ class AlertService {
       const positionSizing = execution?.positionSizing || {};
       const riskPerShare = entryPrice - initialStopLoss;
       const rMultiple = (currentPrice - stopLoss) / (riskPerShare || 1);
-      if (Number.isFinite(rMultiple) && rMultiple >= 2.0) {
+      const isAboveBreakeven = stopLoss > entryPrice;
+      if (Number.isFinite(rMultiple) && (rMultiple >= 2.0 || (isAboveBreakeven && rMultiple >= 1.5))) {
          let multiplier = 1
           if (rMultiple >= 3.0) multiplier = 2
           if (rMultiple >= 4.0) multiplier = 3
